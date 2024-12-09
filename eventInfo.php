6b5b6c46caa82a673c505612796034e669db4d30
sygoh0909 <shuyigoh2004@gmail.com>
2024-12-09 14:46:58 +0800

    updates

--- eventInfo.php
+++ eventInfo.php
@@ -6,7 +6,7 @@ include ('db/db_conn.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upcoming Event Info Page</title>
+    <title>Event Info Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="main.css">
     <style>
