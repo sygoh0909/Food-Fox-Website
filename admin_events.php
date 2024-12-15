2d08c2703ac19c88687cb594ca83975cb28ee552
sygoh0909 <shuyigoh2004@gmail.com>
2024-12-15 12:16:25 +0800

    updates

--- admin_events.php
+++ admin_events.php
@@ -97,7 +97,7 @@ include ('db/db_conn.php');
                         echo "<td>" . $row["eventName"] . "</td>";
                         echo "<td>" . $row["totalRegistrations"] . "</td>";
                         echo "<td><a href='action_event.php?eventID=" .$row['eventID']. "&action=edit'><button>Edit</button></a><a href='action_event.php?eventID=" .$row['eventID']. "&action=delete'><button>Delete</button></a><a href='admin_registrations.php?eventID=" .$row['eventID']."'><button>View Registrations</button></a></td>";
-                        echo "<td><a href='generateLoginForm.php?eventID=" .$row['eventID'] ."'><button>Generate Attendance Link</button></a>";
+                        echo "<td><a href='generateAttendanceLink.php?eventID=" .$row['eventID'] ."'><button>Generate Attendance Link</button></a>";
                         echo "</tr>";
                     }
                 } else {
